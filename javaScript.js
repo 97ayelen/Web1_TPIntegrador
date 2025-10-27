be140990f624d319cca7c584e36d11b720ccc2d9
Julian Esquiaga <julian.esquiaga@gmail.com>
2025-10-27 20:47:56 -0300

    Se agrega logica de carousel en index + js

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,18 @@
-const carrusel = document.querySelectorAll('carousel-item');
+const carrusel = document.querySelectorAll('.carousel-item img');
 let indice = 0;
 
 const img = document.getElementById('imagen');
 const btnSiguiente = document.getElementById('botonSiguiente');
 const btnAtras = document.getElementById('botonAtras');
 
-btnSiguiente.addEventListener("click", () =>{
-    indice = (indice + 1) % carrusel.length;
-    img.src = carrusel[indice];
-})
+
+btnSiguiente.addEventListener("click", () => {
+  indice = (indice + 1) % carrusel.length;
+  img.src = carrusel[indice].getAttribute('src');
+});
+
+btnAtras.addEventListener("click", () => {
+    indice = (indice - 1 + carrusel.length) % carrusel.length;
+    img.src = carrusel[indice].getAttribute('src');
+});
+
