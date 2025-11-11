587491c7fceb1b247eee929b5e759defc44677f6
Julian Esquiaga <julian.esquiaga@gmail.com>
2025-11-11 12:46:09 -0300

    Correccion del TP acorde a comentarios del Profe
    Se implementa logica para que el carrusel tome imagenes desde array

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,13 @@
-const carrusel = document.querySelectorAll('.carousel-item img');
+const imagenes = [
+  "img/fotos/complejoExterior2.jpg",
+  "img/fotos/hotelHabitacion.jpg",
+  "img/fotos/patioParrilla.jpg",
+  "img/fotos/patioPileta.jpg",
+  "img/fotos/cochera.jfif",
+  "img/fotos/paisaje.jfif"
+];//CREAMOS ARRAY CON IMAGENES segun correccion del profe
+
+//const carrusel = document.querySelectorAll('.carousel-item img');
 let indice = 0;
 
 const img = document.getElementById('imagen');
@@ -7,12 +16,23 @@ const btnAtras = document.getElementById('botonAtras');
 
 
 btnSiguiente.addEventListener("click", () => {
-  indice = (indice + 1) % carrusel.length;
-  img.src = carrusel[indice].getAttribute('src');
+  indice = (indice + 1) 
+
+  if (indice >= imagenes.length) { //si indice es igual o mayor al array vuelve de 0
+   indice = 0; 
+}
+
+img.src = imagenes[indice];
 });
 
 btnAtras.addEventListener("click", () => {
-    indice = (indice - 1 + carrusel.length) % carrusel.length;
-    img.src = carrusel[indice].getAttribute('src');
+    indice = (indice - 1);
+
+    if (indice < 0) {
+      indice = imagenes.length -1; //si llega a 0 vuelve a la ultima posicion
+    }
+
+    img.src = imagenes[indice];
+
 });
 
